48911c6b42bf91f9025efeb86efd174e275317fc
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2023-04-03 19:12:58 -0300

    Feat: implemented test in TDD to the route get/teams

--- app/backend/src/tests/team.test.ts
+++ app/backend/src/tests/team.test.ts
@@ -5,6 +5,7 @@ import chaiHttp = require('chai-http');
 
 import { app } from '../app';
 import TeamModel from '../database/models/Team';
+import TeamsMock from './Mocks/team.mock';
 
 import { Response } from 'superagent';
 
@@ -12,34 +13,33 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Seu teste', () => {
+describe('Testes da rota /teams', () => {
   /**
    * Exemplo do uso de stubs com tipos
    */
 
   let chaiHttpResponse: Response;
 
-  // before(async () => {
-  //   sinon
-  //     .stub(TeamModel, "findOne")
-  //     .resolves({
-  //       ...<Seu mock>
-  //     } as TeamModel);
-  // });
+  afterEach(()=>{
+    (TeamModel.findAll as sinon.SinonStub).restore();
+  })
 
-  // after(()=>{
-  //   (TeamModel.findOne as sinon.SinonStub).restore();
-  // })
+  it('Retorna statusHttp 200, ao realizar requisitção get/teams', async () => {
 
-  // it('...', async () => {
-  //   chaiHttpResponse = await chai
-  //      .request(app)
-  //      ...
-
-  //   expect(...)
-  // });
-
-  it('Seu sub-teste', () => {
-    expect(false).to.be.eq(true);
+  before(async () => {
+    sinon
+      .stub(TeamModel, "findAll")
+      .resolves( TeamsMock as TeamModel[]);
   });
+
+    chaiHttpResponse = await chai
+       .request(app).get('/teams');
+
+    expect(chaiHttpResponse.status).to.be('200');
+    expect(chaiHttpResponse.body).to.be.deep.equal(TeamsMock);
+  });
+
+  // it('Seu sub-teste', () => {
+  //   expect(false).to.be.eq(true);
+  // });
 });
