608928f7d27b00971487a23e4a7c9ad0cdea3c62
Romildo Silva Filho <romildo.silvafilho1@gmail.com>
2023-04-07 18:12:55 -0300

    Feat: implemented the route get /leaderboard/home

--- app/backend/src/database/service/LeaderBoardService.ts
+++ app/backend/src/database/service/LeaderBoardService.ts
@@ -10,11 +10,16 @@ export default class LeaderBoardService {
     const matches = await this.matcheModel.findAll({ where: { inProgress: false } });
 
     const homeBoard = teams.map((team) => {
+      const filtredMatches = matches.filter((matche) => matche.homeTeamId === team.id);
       const teamBoard = {
         name: team.teamName,
-        totalPoints: PerformaceCalculate.totalPoints(
-          matches.filter((matche) => matche.homeTeamId === team.id),
-        ),
+        totalPoints: PerformaceCalculate.totalPoints(filtredMatches),
+        totalGames: PerformaceCalculate.totalGames(filtredMatches),
+        totalVictories: PerformaceCalculate.totalVictories(filtredMatches),
+        totalDraws: PerformaceCalculate.totalDraws(filtredMatches),
+        totalLosses: PerformaceCalculate.totalLosses(filtredMatches),
+        goalsFavor: PerformaceCalculate.goalsFavor(filtredMatches),
+        goalsOwn: PerformaceCalculate.goalsOwn(filtredMatches),
       };
       return teamBoard;
     });
--- app/backend/src/database/utils/LeaderBoardCalc.ts
+++ app/backend/src/database/utils/LeaderBoardCalc.ts
@@ -14,4 +14,63 @@ export default class PerformaceCalculate {
     }, 0);
     return totalPoints;
   }
+
+  public static totalGames(matches: Matche[]) {
+    return matches.length;
+  }
+
+  public static totalVictories(matches: Matche[]) {
+    const totalVictories = matches.reduce((acc: number, curr: Matche) => {
+      let victories = 0;
+      if (curr.homeTeamGoals > curr.awayTeamGoals) {
+        victories += 1;
+      }
+      return acc + victories;
+    }, 0);
+    return totalVictories;
+  }
+
+  public static totalDraws(matches: Matche[]) {
+    const totalDraws = matches.reduce((acc: number, curr: Matche) => {
+      let draws = 0;
+      if (curr.homeTeamGoals === curr.awayTeamGoals) {
+        draws += 1;
+      }
+      return acc + draws;
+    }, 0);
+    return totalDraws;
+  }
+
+  public static totalLosses(matches: Matche[]) {
+    const totalLosses = matches.reduce((acc: number, curr: Matche) => {
+      let losses = 0;
+      if (curr.homeTeamGoals < curr.awayTeamGoals) {
+        losses += 1;
+      }
+      return acc + losses;
+    }, 0);
+    return totalLosses;
+  }
+
+  public static goalsFavor(matches: Matche[]) {
+    const goalsFavor = matches.reduce((acc: number, curr: Matche) => {
+      let goals = 0;
+      if (curr.homeTeamGoals) {
+        goals += curr.homeTeamGoals;
+      }
+      return acc + goals;
+    }, 0);
+    return goalsFavor;
+  }
+
+  public static goalsOwn(matches: Matche[]) {
+    const goalsOwn = matches.reduce((acc: number, curr: Matche) => {
+      let goals = 0;
+      if (curr.awayTeamGoals) {
+        goals += curr.awayTeamGoals;
+      }
+      return acc + goals;
+    }, 0);
+    return goalsOwn;
+  }
 }
